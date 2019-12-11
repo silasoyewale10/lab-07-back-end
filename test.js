85e714785895306baf1d1327bdb0f33975da57e8
Silas Oyewale <silasoyewale10@gmail.com>
2019-12-11 13:10:59 -0800

    map working

--- test.js
+++ test.js
@@ -3,31 +3,33 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const superagent = require('superagent');
+
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
 // LOCATION DATA
 
-function FormattedData(query, response) {
+function FormattedData(query, location) {
     this.search_query = query;
-    this.formatted_query = response.body.results[0].formatted_address;
-    this.latitude = response.body.results[0].geometry.location.lat;
-    this.longitude = response.body.results[0].geometry.location.lng;
+    this.formatted_query = location.formatted_address;
+    this.latitude = location.geometry.location.lat;
+    this.longitude = location.geometry.location.lng;
 }
 
 
 app.get('/location', handleLocationRequest)
+
 function handleLocationRequest(request, response) {
-    // const quer = request.query.data;
-    superagent.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`
-        ).then(result => {
-    const place = new FormattedData (request.query.data, result);
-    response.send(place)
-})
-.catch(err => handleError (err, response))
+
+    let query = request.query.data;
+
+    const interestedData = require('./data/geo.json')
+console.log(interestedData.results[0])
+    let newLocation = new FormattedData(query, interestedData.results[0]) 
+
+    response.send(newLocation)
 }
+
 
 function FormattedTimeAndWeather(query, specificweather) {
     
@@ -35,7 +37,23 @@ function FormattedTimeAndWeather(query, specificweather) {
     this.time = new Date(specificweather.time * 1000).toDateString();
 }
 
+
 app.get('/weather', handleWeatherRequest)
+
+// function handleWeatherRequest(request, response) {
+//     var arrDaysWeather = [];
+//     let query = request.query.data;
+//     const weatherData = require('./data/darksky.json')
+//     // console.log(weatherData.daily.data.length)
+
+//     for (var x = 0; x < weatherData.daily.data.length; x++){
+//         console.log("weatherData.daily.data[x] is " + weatherData.daily.data[x])
+//         var newWeather = new FormattedTimeAndWeather(query, weatherData.daily.data[x])
+//         arrDaysWeather.push(newWeather)
+//     }
+//     response.send(arrDaysWeather)
+// }
+
 function handleWeatherRequest(request, response) {
     var arrDaysWeather = [];
     let query = request.query.data;
@@ -52,13 +70,9 @@ function handleWeatherRequest(request, response) {
     
     response.send(arrDaysWeather)
 }
-
-  function handleError(err, response) {
-    console.log(err);
-    if (response) response.status(500).send('You are wrong. Merry Christmas');
-  }
-
-
+app.get('/*', function(request, response){
+    response.status(404).send('Error Loading Results')
+  })
 
 console.log('LOCATIONS END FIRING');
 
