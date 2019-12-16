1a83d2dd2e185618388ce1c1592a92081ec81d96
Silas Oyewale <silasoyewale10@gmail.com>
2019-12-16 12:19:44 -0800

    done

--- server.js
+++ server.js
@@ -8,14 +8,10 @@ const superagent = require('superagent');
 const app = express();
 app.use(cors());
 
-
-
-
+// const client = require('./utils.js').client;
 
 const PORT = process.env.PORT || 3000;
 // LOCATION DATA
-
-
 const handleLocationRequest = require('./modules/location');
 const getWeather = require('./modules/weather')
 const getEvents = require('./modules/event')
@@ -26,10 +22,7 @@ app.get('/events', getEvents);
 app.get('/movies', getMovie);
 app.get('/location', handleLocationRequest)
 // app.get('/yelp', getWeather);
-
-
 app.get('/', (req, res) => {
-
 	//check the database
 	const SQL = 'SELECT * FROM locations;';
 	client.query(SQL).then(sqlResponse => {
@@ -38,19 +31,13 @@ app.get('/', (req, res) => {
 		var currentCity = 'SELECT * FROM locations WHERE location = $1';
 	});
 })
-
-
 app.get('/yelp', getYelp);
-
 
 function handleError(err, response) {
 	// console.log(err);
 	if (response) response.status(500).send('You are wrong. Merry Christmas');
 
 }
-
-// console.log('LOCATIONS END FIRING');
-
 app.listen(PORT, () => {
 	console.log("Port is working and listening  onnnnn port " + PORT)
 });
